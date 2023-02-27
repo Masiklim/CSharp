207205889d48c8c19effb12abebd344402a3bb3d
Maksim Manchinsky <machinsky19947@gmail.com>
2023-02-27 17:29:56 +0300

    dz zadacha 4

---
+++ dzsem1_4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите целое число 1: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите целое число 2: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите целое число 3: ");
+int с = int.Parse(Console.ReadLine());
+
+max = Math.Max(a, Math.Max(b, c));
+Console.WriteLine(max);
+
+
