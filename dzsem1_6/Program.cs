3f0568644c0a93fcec99a02d58431f43f1828f0e
Maksim Manchinsky <machinsky19947@gmail.com>
2023-02-27 18:14:09 +0300

    проверка на четность чисел

---
+++ dzsem1_6/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число я овечу чётное ли оно!");
+Console.Write("Введите целое число: ");
+int NamA = int.Parse(Console.ReadLine());
+
+if (NamA%2==0)
+{
+    Console.WriteLine($" -> Да");
+}
+else 
+{
+   Console.WriteLine($"-> Нет"); 
+}
+
+//($-> "");
