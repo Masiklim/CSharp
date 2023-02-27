cc4aad347d1ac6cec2cfa935b682cace3dd4538e
Maksim Manchinsky <machinsky19947@gmail.com>
2023-02-27 15:55:52 +0300

    zadanie na proverku kvdrata chisla da net

---
+++ zadanie12/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите целое число: ");
+int NamA = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите целое число: ");
+int NamB = int.Parse(Console.ReadLine() ?? "");
+
+if (NamA==NamB*NamB)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+//int result = (int)Math.Pow(userNamber, 2);// открыли математическую функцию, выбрали Pow и после написали в какую степень возводить
+
+//Console.WriteLine($"{userNamber} -> {result}");
