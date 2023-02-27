72fa4275a7a8b693296db57cd4ef187498d15efc
Maksim Manchinsky <machinsky19947@gmail.com>
2023-02-27 14:47:03 +0300

    sem1

---
+++ sem1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите целое число: ");
+
+
+int userNamber = int.Parse(Console.ReadLine() ?? "");
+
+int result = (int)Math.Pow(userNamber, 2);// открыли математическую функцию, выбрали Pow и после написали в какую степень возводить
+
+Console.WriteLine($"{userNamber} -> {result}");// красивый вывод
