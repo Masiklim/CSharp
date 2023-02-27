fd3e4a6040977ff8b19615b4ec228c56af7b49a0
Maksim Manchinsky <machinsky19947@gmail.com>
2023-02-27 17:53:44 +0300

    DZ zadanie 2

---
+++ dzsem1_2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите целое число: ");
+int NamA = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите целое число: ");
+int NamB = int.Parse(Console.ReadLine() ?? "");
+
+if (NamA > NamB)
+{
+    Console.WriteLine(NamA);
+}
+else 
+{
+    Console.WriteLine($" -> {NamB}");
+}
