4e2d211bca9c820899ea1fdbe83c75865e72ca4a
Maksim Manchinsky <machinsky19947@gmail.com>
2023-02-27 17:07:19 +0300

    dni nedeli

---
+++ zadanie12_2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите  число: ");
+int week = int.Parse(Console.ReadLine() ?? "");
+
+if ((week<1) | (week>7))
+{
+    Console.WriteLine("Неверное число");
+}
+else 
+{
+    if (week==1) {Console.Write(" - Понедельник ");}
+    if (week==2) {Console.Write(" - Вторник ");}
+    if (week==3) {Console.Write(" - Среда ");}
+    if (week==4) {Console.Write(" - Четверг ");}
+    if (week==5) {Console.Write(" - Пятница");}
+    if (week==6) {Console.Write(" - Суббота ");}
+    if (week==7) {Console.Write(" - Воскресенье ");}
+    
+}
+
