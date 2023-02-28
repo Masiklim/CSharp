5e1ad2986af5cc99ce3af48abf2e6cc35cb1d6f6
Maksim Manchinsky <machinsky19947@gmail.com>
2023-02-28 11:28:47 +0300

    dz 1 zadacha 8

---
+++ dzsem1_8/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int evenNumber = 2;
+
+if(number > 1)
+{
+    while(evenNumber <= number)
+    {
+        Console.Write(evenNumber + " ");
+        evenNumber = evenNumber + 2;
+    }
+}
+
+//($-> "");
+//(NamA%2==0)
+//Console.WriteLine($"-> Нет"); 
