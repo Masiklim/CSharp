3dd6eb2a23f929265898276c5c55e6b19924b5c7
Maksim Manchinsky <machinsky19947@gmail.com>
2023-03-05 17:07:17 +0300

    циклический ввод переменной

---
+++ sem2/СiklWritA..cs
@@ -0,0 +1,12 @@
+﻿// циклический ввод переменной, для инт просто поменять тип
+//int 
+double varA;
+while (true)
+{
+    Console.Write("Введите А: ");
+       //int    
+    if (double.TryParse(Console.ReadLine(), out varA))
+        break;
+        Console.WriteLine("Ошибка Ввода!");
+}
+Console.WriteLine($"A={varA}");
